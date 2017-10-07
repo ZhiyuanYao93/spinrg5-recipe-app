2b7ffb59afa2563205c120c4bbc8dcf91be5316a
Zhiyuan Yao <yaozyforwork@gmail.com>
2017-10-07 00:11:11 -0700

    add function to view ingredients of a recipe.

---
+++ src/test/java/com/zhiyuan/spinrg5recipeapp/controllers/IngredientControllerTest.java
@@ -0,0 +1,54 @@
+package com.zhiyuan.spinrg5recipeapp.controllers;
+
+import com.zhiyuan.spinrg5recipeapp.commands.RecipeCommand;
+import com.zhiyuan.spinrg5recipeapp.services.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+public class IngredientControllerTest {
+    @Mock
+    RecipeService recipeService;
+
+    IngredientController ingredientController;
+    MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        this.ingredientController = new IngredientController(recipeService);
+
+        mockMvc = MockMvcBuilders.standaloneSetup(ingredientController).build();
+    }
+
+    @Test
+    public void testListIngredients() throws Exception {
+        //given
+        RecipeCommand recipeCommand = new RecipeCommand();
+        when(recipeService.findRecipeCommandById(anyLong())).thenReturn(recipeCommand);
+
+        //when
+        mockMvc.perform(get("/recipe/1/ingredients"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/ingredient/list"))
+                .andExpect(model().attributeExists("recipe"));
+
+
+        //then
+        verify(recipeService,times(1)).findRecipeCommandById(anyLong());
+    }
+}
