8f6cd99706222e0297a23c8f34d3aa3eb5e934ab
Zhiyuan Yao <yaozyforwork@gmail.com>
2017-10-09 13:36:53 -0700

    Spring Exception handler, add 404error page

--- src/main/java/com/zhiyuan/spinrg5recipeapp/controllers/RecipeController.java
+++ src/main/java/com/zhiyuan/spinrg5recipeapp/controllers/RecipeController.java
@@ -1,12 +1,16 @@
 package com.zhiyuan.spinrg5recipeapp.controllers;
 
 import com.zhiyuan.spinrg5recipeapp.commands.RecipeCommand;
+import com.zhiyuan.spinrg5recipeapp.exceptions.NotFoundException;
 import com.zhiyuan.spinrg5recipeapp.services.RecipeService;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @Controller
@@ -55,6 +59,18 @@ public class RecipeController {
         recipeService.deleteById(Long.valueOf(id));
 
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(){
+        log.error("Handling Not Found Error");
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("404error");
+
+        return modelAndView;
 
     }
 
--- src/test/java/com/zhiyuan/spinrg5recipeapp/controllers/RecipeControllerTest.java
+++ src/test/java/com/zhiyuan/spinrg5recipeapp/controllers/RecipeControllerTest.java
@@ -56,13 +56,12 @@ public class RecipeControllerTest {
 
     @Test
     public void testShowIdNotFound() throws Exception {
-        Recipe recipe = new Recipe();
-        recipe.setId(1L);
-
         when(recipeService.findById(anyLong())).thenThrow(NotFoundException.class);
 
         mockMvc.perform(get("/recipe/1/show"))
-                .andExpect(status().isNotFound());
+                .andExpect(status().isNotFound())
+                .andExpect(view().name("404error"));
+
     }
 
     @Test
